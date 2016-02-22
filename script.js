62ab240fb4684f7665f39641c5c24cf10e3ab79a
Toni Langley <chibia2@gmail.com>
2016-02-22 11:54:22 -0500

    on click: right answers-green; wrong answers-red

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 //displaying the answer
-
 var ans = document.querySelector(".answer");
+var wron = document.querySelectorAll("p");
+
 ans.addEventListener("click", displayAnswer);
 
+
+
 function displayAnswer(){
-ans.style.color = "green"; //change to hex color
+  //incorrect answers
+  /*wron isn't an array but a html collection that is array like
+    [] is needed to gain access to forEach; .call changes this from [] to wron*/
+  [].forEach.call(wron, function(wron){
+    wron.style.color = "red";
+  })
+  //correct answers
+  ans.style.color = "green"; //change to hex color
 }
+
+//make a play again function
