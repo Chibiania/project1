e4425b8144d0d54a64c1cc0795166141d271c0f8
Toni Langley <chibia2@gmail.com>
2016-02-22 11:15:26 -0500

    created displayAnswer function

---
+++ script.js
@@ -0,0 +1,8 @@
+//displaying the answer
+
+var ans = document.querySelector(".answer");
+ans.addEventListener("click", displayAnswer);
+
+function displayAnswer(){
+ans.style.color = "green"; //change to hex color
+}
