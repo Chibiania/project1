3d2f815ab8c77847d06355bee62843b645e7777e
Toni Langley <chibia2@gmail.com>
2016-02-22 15:42:25 -0500

    able to cycle through array to print questions

--- script.js
+++ script.js
@@ -1,49 +1,69 @@
-//displaying the answer
+//
+// var questions = [
+//   {question: "What color is the sky?", rightAnswer: "Blue", wrongAnswer: "Jose"}
+// ]
+// questions.forEach(function(question){
+//   var element = document.createElement("DIV");
+//   element.textContent = question.question;
+//   document.body.appendChild(element);
+// });
 
-//need to cycle through the questions, determine which is the question being worked on, and display answers appropriately
+var questions = [
+  {
+    quest: "Q1",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  },
+  {
+    quest: "Q2",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  },
+  {
+    quest: "Q3",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  },
+  {
+    quest: "Q4",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  },
+  {
+    quest: "Q5",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  },
+  {
+    quest: "Q6",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  },
+  {
+    quest: "Q7",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  },
+  {
+    quest: "Q8",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  },
+  {
+    quest: "Q9",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  },
+  {
+    quest: "Q10",
+    rightAnswer: "Correct",
+    wrongAnswer: "Wrong"
+  }
+]
 
-//for Q1
-//is there a way to cycle through sections with switch or is if the best solution?
-var quest = document.querySelector("section:nth-child(1)");
-var ans = document.querySelector(".answer");
-var wron = document.querySelector("section:nth-child(1)").querySelectorAll(".wrong");
-var allWron = document.querySelectorAll(".wrong")[1];
-var justWron = document.querySelector(".wrong");
-
-if(allWron.addEventListener("click", displayAnswer)){
-  //wrongAnswer();
-  // console.log("msbjafbj");
-  // wrongAnswerB();
-}
-else if (justWron.addEventListener("click", displayAnswer)){
-  // wrongAnswer();
-}
-//allWron.addEventListener("click", displayAnswer) || justWron.addEventListener("click", displayAnswer);
-
-
-//points to last answer
-function wrongAnswerB(){
-  var newP = document.createElement("p");
-  var someText = document.createTextNode("^^^^^^^^You choose this answer");
-  newP.appendChild(someText);
-
- allWron.parentNode.insertBefore(newP, allWron.nextSibling);
-
-  //document.section.insertBefore(newP, allWron);
-
-}
-
-function displayAnswer(){
-  //incorrect answers
-  /*wron isn't an array but a html collection that is array like
-  //   [] is needed to gain access to forEach; .call changes this from [] to wron*/
-  // [].forEach.call(wron, function(wron){
-    wron.style.color = "red";
-  // })
-  //correct answers
-  ans.style.color = "green"; //change to hex color
-
-  //wrongAnswerB();
-}
-
-//make a play again function
+//goes through questions array and prints out each question
+questions.forEach(function(insertQuestion){
+  var newElement = document.createElement("section");
+  newElement.textContent = insertQuestion.quest;
+  document.body.appendChild(newElement);
+});
