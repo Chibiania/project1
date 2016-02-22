c428ac45731434def59ea637b0190d48963d810e
Toni Langley <chibia2@gmail.com>
2016-02-22 16:43:44 -0500

    assigned all question chunks to individual divs

--- script.js
+++ script.js
@@ -61,29 +61,44 @@ var questions = [
     wrongAnswerB: "Wrong"
   }
 ]
+var i=1;
 
 //goes through questions array and prints out each question
 questions.forEach(function(insertQuestion){
+  var element = document.createElement("div");
+  document.body.appendChild(element);
+  element.setAttribute("id", "div"+i);
+
   var questElement = document.createElement("h2");
   questElement.textContent = insertQuestion.quest;
-  document.body.appendChild(questElement);
-
+  document.getElementById("div"+i).appendChild(questElement);
 
   //put correct answers in
   var ansElement = document.createElement("p");
   ansElement.textContent = insertQuestion.rightAnswer;
-  document.body.appendChild(ansElement);
+  document.getElementById("div"+i).appendChild(ansElement);
 
   //puts in wrong answer A
   var wrongAElement = document.createElement("p");
   wrongAElement.textContent = insertQuestion.wrongAnswerA;
-  document.body.appendChild(wrongAElement);
+  document.getElementById("div"+i).appendChild(wrongAElement);
 
   //puts in wrong answer B
   var wrongBElement = document.createElement("p");
   wrongBElement.textContent = insertQuestion.wrongAnswerB;
-  document.body.appendChild(wrongBElement);
+  document.getElementById("div"+i).appendChild(wrongBElement);
+  i++;
+
 });
 
+//do things to colors based on click action
+//when something is clicked, change the color
+//.addEventListener("click", someFucntionToChangeColor);
 
-//display answers
+function test() {
+
+    var element = document.createElement("div");
+    element.appendChild(document.createTextNode('The man who mistook his wife for a hat'));
+    document.getElementById('lc').appendChild(element);
+
+}
