0e4953e469aaab2f8266c03b3188490e078f8a40
Toni Langley <chibia2@gmail.com>
2016-02-23 16:53:03 -0500

    trying to get increments

--- script.js
+++ script.js
@@ -65,39 +65,44 @@ var i=1;
 
 //goes through questions array and prints out each question
 questions.forEach(function(insertQuestion){
+
+  //empty div
   var element = document.createElement("div");
   document.body.appendChild(element);
   element.setAttribute("id", "div"+i);
-  $(element).one("click", colorChange);
+  element.addEventListener("click", colorChange);
+
+  //questions
   var questElement = document.createElement("h2");
   questElement.textContent = insertQuestion.quest;
   document.getElementById("div"+i).appendChild(questElement);
 
-  //button div
-  var buttonDiv = document.createElement("div");
-  document.body.appendChild(buttonDiv);
-  buttonDiv.setAttribute("id", "buttons");
-  $(buttonDiv).one("click", right);
+  //button section
+  var buttonSection = document.createElement("section");
+  document.body.appendChild(buttonSection);
+  buttonSection.setAttribute("id", "buttons"+i);
+  document.getElementById("div"+i).appendChild(buttonSection);
+  $(buttonSection).one("click", right);
 
   //right button = correct answer
   var ansElement = document.createElement("button");
   ansElement.textContent = insertQuestion.rightAnswer;
   ansElement.setAttribute("class", "correct");
-  document.getElementById("div"+i).appendChild(ansElement);
+  document.getElementById("buttons"+i).appendChild(ansElement);
 
   //wrong button A = wrong answer A
   var wrongAElement = document.createElement("button");
   wrongAElement.textContent = insertQuestion.wrongAnswerA;
   wrongAElement.setAttribute("class", "wrongA");
   wrongAElement.setAttribute("type", "radio");
-  document.getElementById("div"+i).appendChild(wrongAElement);
+  document.getElementById("buttons"+i).appendChild(wrongAElement);
 
   //wrong button B = wrong answer B
   var wrongBElement = document.createElement("button");
   wrongBElement.textContent = insertQuestion.wrongAnswerB;
   wrongBElement.setAttribute("class", "wrongB");
   wrongBElement.setAttribute("type", "radio");
-  document.getElementById("div"+i).appendChild(wrongBElement);
+  document.getElementById("buttons"+i).appendChild(wrongBElement);
   i++;
 });
 
@@ -137,7 +142,6 @@ $("#next").click(function () {
 //do things to colors based on click action
 //when something is clicked, change the color
 function colorChange(){
-  //$("").one("click", right);
   this.querySelector(".correct").style.backgroundColor = "green";
   this.querySelector(".wrongA").style.backgroundColor = "red";
   this.querySelector(".wrongB").style.backgroundColor = "red";
@@ -146,6 +150,15 @@ function colorChange(){
   //use innerText or innerHtml to compare clicked values
 }
 
+var yes = 0;
+function right(){
+
+  //if .correct is chosen, increment yes
+  //if(document.querySelector(".correct").innerText==questions[0].rightAnswer){
+  yes++;
+  console.log("Yes: " + yes);
+//}
+}
 
 //questions should be displayed randomly
 
