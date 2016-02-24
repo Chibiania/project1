ae95e16b45eb3ade327ef5db3839f9e850dfd984
Toni Langley <chibia2@gmail.com>
2016-02-24 15:04:23 -0500

    trying to get dataset if/else to work

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //all the questions
 var questions = [
   {
-    quest: "Year 1: What does the Mirror of Erised's inscription mean? <br> \"Erised stra ehru oyt ube cafru oyt on wohsi.\"",
+    quest: "Year 1: What does the Mirror of Erised's inscription mean?" +"<br />"+ "\"Erised stra ehru oyt ube cafru oyt on wohsi.\"",
     rightAnswer: "I show not your face but your heart's desire",
     wrongAnswerA: "Come, I will show you all the things you wish for",
     wrongAnswerB: "This is not a mirror for viewing one's self, but to bestow relevation"
@@ -26,7 +26,7 @@ var questions = [
   },
   {
     //
-    quest: "\"The one with the power to vanquish the Dark Lord approaches... born to those who have thrice defied him, born as the seventh month dies... and the Dark Lord will mark him as his equal, but he will have power the Dark Lord knows not... and either must die at the hand of the other for neither can live while the other survives... the one with the power to vanquish the Dark Lord will be born as the seventh month dies....\"\n -Sybill Trelawney's prophecy made to Albus Dumbledore\n Year 5: Who does this apply to?",
+    quest: "Year 5: Who does this apply to?" + "<br />"+ "<br />"+ "\"The one with the power to vanquish the Dark Lord approaches... born to those who have thrice defied him, born as the seventh month dies... and the Dark Lord will mark him as his equal, but he will have power the Dark Lord knows not... and either must die at the hand of the other for neither can live while the other survives... the one with the power to vanquish the Dark Lord will be born as the seventh month dies....\"" +"<br />"+ "—Sybill Trelawney's prophecy made to Albus Dumbledore",
     rightAnswer: "Harry Potter and Neville Longbottom",
     wrongAnswerA: "Harry Potter",
     wrongAnswerB: "Neville Longbottom"
@@ -61,7 +61,7 @@ var questions = [
     wrongAnswerA: "Godric Griffindor's sword, Slytherin's locket, Fawkes, Hermoine's time turner, Albus Dumbledore's pensieve, Triwizard Trophy, and the Kinght Bus",
     wrongAnswerB: "Dobby, Kreacher, Winky, Hokey, Hooky, Manker, and Wockey"
   }
-]
+];
 var i=1;
 
 //goes through questions array and prints out each question
@@ -75,7 +75,7 @@ questions.forEach(function(insertQuestion){
 
   //questions
   var questElement = document.createElement("h2");
-  questElement.textContent = insertQuestion.quest;
+  questElement.innerHTML = insertQuestion.quest;
   document.getElementById("div"+i).appendChild(questElement);
 
   //button section
@@ -89,12 +89,14 @@ questions.forEach(function(insertQuestion){
   var ansElement = document.createElement("button");
   ansElement.textContent = insertQuestion.rightAnswer;
   ansElement.setAttribute("class", "correct");
+  ansElement.setAttribute("data-right", "correct");
   document.getElementById("buttons"+i).appendChild(ansElement);
 
   //wrong button A = wrong answer A
   var wrongAElement = document.createElement("button");
   wrongAElement.textContent = insertQuestion.wrongAnswerA;
   wrongAElement.setAttribute("class", "wrongA");
+  wrongAElement.setAttribute("data-wrong", "incorrect");
   wrongAElement.setAttribute("type", "radio");
   document.getElementById("buttons"+i).appendChild(wrongAElement);
 
@@ -102,6 +104,7 @@ questions.forEach(function(insertQuestion){
   var wrongBElement = document.createElement("button");
   wrongBElement.textContent = insertQuestion.wrongAnswerB;
   wrongBElement.setAttribute("class", "wrongB");
+  wrongBElement.setAttribute("data-wrong", "incorrect");
   wrongBElement.setAttribute("type", "radio");
   document.getElementById("buttons"+i).appendChild(wrongBElement);
   i++;
@@ -144,13 +147,24 @@ $("#next").click(function () {
 //do things to colors based on click action
 //when something is clicked, change the color
 function colorChange(){
-  this.querySelector(".correct").style.backgroundColor = "green";
-  this.querySelector(".wrongA").style.backgroundColor = "red";
-  this.querySelector(".wrongB").style.backgroundColor = "red";
 
-  right();
+  //make it so that whatever answer is picked, THAT is the one that changes color
+  this.querySelector(".correct").style.backgroundColor = "rgba(0, 128, 0, .65)";
+  this.querySelector(".wrongA").style.backgroundColor = "rgba(255, 0, 0, .65)";
+  this.querySelector(".wrongB").style.backgroundColor = "rgba(255, 0, 0, .65)";
 
-//idk if this can get the increments right or not
+  if((this.querySelector(".correct").dataset.right)=="correct"){
+
+    yes++;
+    console.log("Yes: " + yes);
+
+  }else if((this.querySelector(".wrongA").dataset.wrong)=="incorrect"){
+    console.log("I'm the wrongA answer!");
+    
+  }else if((this.querySelector(".wrongB").dataset.wrong)=="incorrect"){
+    console.log("I'm the wrongB answer!");
+  }
+  //idk if this can get the increments right or not
 
   //   $(this.querySelector(".correct")).click(function(){
   //   if($(this).prop("class") == "correct"){
@@ -170,13 +184,14 @@ function right(){
   //if .correct is chosen, increment yes
   //if(document.querySelector(".correct").innerText==questions[0].rightAnswer){
 
+  if((document.querySelector(".correct").dataset.correct)=="correct"){
 
 
-
-  yes++;
-  console.log("Yes: " + yes);
+    yes++;
+    console.log("Yes: " + yes);
+  }
   //return yes;
-//}
+  //}
 }
 
 //questions should be displayed randomly
